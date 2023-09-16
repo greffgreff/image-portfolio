a92e4bae569cb8b69f5caa7a99824bd3b5a2c3ab
greffgreff <greffchandler80@gmail.com>
2023-09-16 02:51:20 +0200

    Added tolerance checks

--- src/pages/Exhibit/index.tsx
+++ src/pages/Exhibit/index.tsx
@@ -1,55 +1,89 @@
 import './style.css'
 import { useEffect } from 'react'
 
+interface Point {
+  x: number
+  y: number
+}
+
+interface Line {
+  lineaFrunction: LinearFunction
+  pos1: Point
+  pos2: Point
+}
+
+interface LinearFunction {
+  slope: number
+  intercept: number
+  fx: (x: number) => Point
+}
+
 export default () => {
   useEffect(() => {
     const container = window.document.getElementById('exhibit-container') as HTMLDivElement
     const boundary = container.getBoundingClientRect()
 
-    const between = (min: number, max: number) => min + Math.random() * (max - min)
+    const createFunction = (): LinearFunction => {
+      const slope = (Math.random() < 0.5 ? -1 : 1) * Math.random()
 
-    const newFx = () => {
-      const a = (Math.random() < 0.5 ? -1 : 1) * Math.random()
-      const lowerY = boundary.height * 0.1
-      const upperY = boundary.height * 0.9
-      const b = between(lowerY, upperY)
+      const lowerY = boundary.height * 0.2
+      const upperY = boundary.height * 0.8
+      const intercept = getBetween(lowerY, upperY)
+
       const fx = (x: number) => {
-        let y = a * x + b
+        let y = slope * x + intercept
         if (y < 0) {
           y = 0
-          x = -b / a
+          x = -intercept / slope
         }
         if (y > boundary.height) {
           y = boundary.height
-          x = (boundary.height - b) / a
+          x = (boundary.height - intercept) / slope
         }
         return { x, y }
       }
-      return { slope: a, intercept: b, fx }
+      
+      return { slope, intercept, fx }
     }
 
-    console.log(boundary.width, boundary.height)
+    const lines: Line[] = []
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
     svg.setAttribute('width', boundary.width.toString())
     svg.setAttribute('height', boundary.height.toString())
 
-    for (let i = 0; i < 10; i++) {
-      const { fx } = newFx()
-      const pos1 = fx(0)
-      const pos2 = fx(boundary.width)
+    let iteration = 0
+    while (lines.length < 4) {
+      if (iteration >= 12) {
+        break
+      }
+      iteration += 1
 
-      const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-      line.setAttribute('x1', pos1.x.toString())
-      line.setAttribute('y1', pos1.y.toString())
-      line.setAttribute('x2', pos2.x.toString())
-      line.setAttribute('y2', pos2.y.toString())
-      line.setAttribute('stroke', randomRgb())
-      line.setAttribute('stroke-width', '2')
+      const linearFunction = createFunction()
+      const pos1 = linearFunction.fx(0)
+      const pos2 = linearFunction.fx(boundary.width)
 
-      svg.appendChild(line)
+      const hasSimilarSlope = lines.some(line =>
+        isWithin(line.lineaFrunction.intercept, linearFunction.intercept, 0.2)
+      )
+      const hasSimilarIntercept = lines.some(line =>
+        isWithin(line.lineaFrunction.slope, linearFunction.slope, 0.1)
+      )
+
+      if (!hasSimilarSlope && !hasSimilarIntercept) {
+        console.log('pushing new line')
+        lines.push({ pos1, pos2, lineaFrunction: linearFunction })
+
+        const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+        line.setAttribute('x1', pos1.x.toString())
+        line.setAttribute('y1', pos1.y.toString())
+        line.setAttribute('x2', pos2.x.toString())
+        line.setAttribute('y2', pos2.y.toString())
+        line.setAttribute('stroke', randomRgb())
+        line.setAttribute('stroke-width', '2')
+        svg.appendChild(line)
+      }
     }
-
     container.appendChild(svg)
   }, [])
 
@@ -61,4 +95,14 @@ function randomRgb() {
   const green = Math.floor(Math.random() * 256)
   const blue = Math.floor(Math.random() * 256)
   return `rgb(${red}, ${green}, ${blue})`
+}
+
+function isWithin(toCheck: number, reference: number, tolerance: number) {
+  const lowerBound = reference * (1 - tolerance)
+  const upperBound = reference * (1 + tolerance)
+  return toCheck >= lowerBound && toCheck <= upperBound
+}
+
+function getBetween(min: number, max: number) {
+  return min + Math.random() * (max - min)
 }
